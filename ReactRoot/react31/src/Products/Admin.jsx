504eb30759ded849840ce4cb484190267444614f
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2024-04-03 19:35:49 +0530

    okie

--- ReactRoot/react31/src/Products/Admin.jsx
+++ ReactRoot/react31/src/Products/Admin.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+import {Link} from 'react-router-dom'
 const Admin = () => {
+  let [products, setProducts] = useState([])
+  useEffect(()=>{
+    getProducts()
+  },[])
+
+  let getProducts=()=>{
+    Axios.get('http://127.0.0.1:5000/api/products')
+    .then((resp)=>{
+      setProducts(resp.data)
+    })
+    .catch()
+  }
+
+  let delProduct = (id)=>{
+    let url = `http://127.0.0.1:5000/api/products/${id}`
+    Axios.delete(url)
+    .then((resp)=>{
+      getProducts()
+    })
+    .catch()
+  }
   return <>
-  <h2>Admin component</h2>
+    <pre>{JSON.stringify(products)}</pre>
+    <div className="container">
+      <div className="row">
+      <div className="col-md-8">
+      <table className='table'>
+      <thead>
+      <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Qty</th>
+          <th>Image</th>
+          <th>Action</th>
+        </tr>
+      </thead>  
+      <tbody>
+        {
+          products.length>0? <>
+          {
+            products.map((product)=>{
+              return <tr>
+                      <td>{product.name}</td>
+                      <td>{product.price}</td>
+                      <td>{product.qty}</td>
+                      <td><img src={product.image} height={"20px"} alt="" /></td>
+                      <td><Link className='btn btn-warning'>Update</Link> <Link  className='btn btn-danger' onClick={delProduct.bind(null,product._id)}>Delete</Link></td>
+                    </tr>
+            })
+          }
+          </>:<h3>No Data</h3>
+        }
+      </tbody>
+      </table>
+      </div>
+      </div>
+    </div>
   </>
 }
 
--- ReactRoot/react31/src/Products/CreateProduct.jsx
+++ ReactRoot/react31/src/Products/CreateProduct.jsx
@@ -1,42 +1,80 @@
 import React, { useState } from 'react'
-
+import {useNavigate} from 'react-router-dom'
+import Axios from 'axios'
 const CreateProduct = () => {
   let [product, setProduct] = useState({"name":"","image":"", "qty":0, "price":0, "info":""})
+  let [status,setStatus] = useState(false);
+  let navigate =useNavigate() ;
+  let imgHandler =(event)=>{
+    let imgFile = event.target.files[0];
+    let reader  = new FileReader()
+    reader.readAsDataURL(imgFile);
+    reader.addEventListener("load",()=>{
+      if(reader.result){
+        setProduct({...product,image:reader.result})
+      }
+      else{
+
+      }
+    })
+
+  }
+  let updateHandler = (event)=>{
+    setProduct({...product,[event.target.name]:event.target.value})
+  }
+  let submitHandler=(event)=>{
+      event.preventDefault()
+      Axios.post('http://127.0.0.1:5000/api/products', product)
+      .then((resp)=>{
+        setStatus(true)
+        //navigate('/admin')
+      })
+      .catch(()=>{})
+  }
   return <>
     <div className="container mt-5">
-    <pre>{JSON.stringify(product)}</pre>
+        <pre>{JSON.stringify(product)}</pre>
       <div className="row">
-      <div className="col-md-6">
-      <div className="card">
-      <div className="card-header">
-          <h4>Upload Product</h4>
-        </div>
-        <div className="card-body">
-          <form >
-            <div className='form-group'>
-              <input className='form-control' type="text" placeholder='Product Name' />
-            </div>
-          
-            <div className='form-group'>
-              <input className='form-control' type="file"/>
-            </div>
-            <div className='form-group'>
-              <input className='form-control' type="number" placeholder='Price' />
-            </div>
-            <div className='form-group'>
-              <input className='form-control' type="number" placeholder='QTY' />
-            </div>
-            <div className='form-group'>
-              <input className='form-control' type="text" placeholder='Information' />
-            </div>
-            <input type="submit" value={"Upload"} className='btn btn-warning' />
-          </form>
-        </div>
-       </div>
-       
 
-      </div>
-      
+        {
+          status?  <>
+            {
+              navigate("/admin")
+            }
+            </> :  <>
+          <div className="col-md-6">
+          <div className="card">
+          <div className="card-header">
+              <h4>Upload Product</h4>
+            </div>
+            <div className="card-body">
+              <form  onSubmit={submitHandler}>
+                <div className='form-group'>
+                  <input onChange={updateHandler} name="name" className='form-control' type="text" placeholder='Product Name' />
+                </div>
+              
+                <div className='form-group'>
+                  <input onChange={imgHandler} name="image" className='form-control' type="file"/>
+                </div>
+                <div className='form-group'>
+                  <input onChange={updateHandler} name="price" className='form-control' type="number" placeholder='Price' />
+                </div>
+                <div className='form-group'>
+                  <input onChange={updateHandler} name="qty" className='form-control' type="number" placeholder='QTY' />
+                </div>
+                <div className='form-group'>
+                  <input onChange={updateHandler} name='info' className='form-control' type="text" placeholder='Information' />
+                </div>
+                <input type="submit" value={"Upload"} className='btn btn-warning' />
+              </form>
+            </div>
+           </div>
+           
+    
+          </div>
+          
+          </>
+        }
       </div>
     </div>
   </>
